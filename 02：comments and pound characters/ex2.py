6a970e5f24dc262f5cd797ff286f74a5c32feead
cJie-sg <72949663+cJie-sg@users.noreply.github.com>
2021-07-06 16:29:04 +0800

    Update ex2.py

--- 02：comments and pound characters/ex2.py
+++ 02：comments and pound characters/ex2.py
@@ -1,3 +1,4 @@
+# 适当在适当的位置添加备注,这样以后在找错误时会比较省事!
 # A comment, this is so you can read your program laterself.
 # Angthing after the # is ignored by python.
 print("I could have # code like this.") # and the comment after is ignored
